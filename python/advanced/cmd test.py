a4b2bd34af2cc17035828db1263d746577b565f6
pyguy9x <lyminhtl@gmail.com>
2021-07-07 16:51:36 +0700

    Thêm ghi chú

--- python/advanced/cmd test.py
+++ python/advanced/cmd test.py
@@ -1,4 +1,5 @@
 import subprocess
-subprocess.call('whoami', shell=True) # cmd
+# Thực thi lệnh command line bằng python
+subprocess.call('whoami', shell=True)  # cmd
 # Output
 # desktop-orf9jgv\minkul
